25b75972058e52044b8e75b913b0dcb60c7ea9c6
raaghavendrahm <raaghavendrahm@gmail.com>
2021-11-04 08:23:40 +0530

    Home, About, and Navbar components are created and embedded in App. "react-router-dom" is installed to enable navigation (routing). With version 6 of react-router-dom, "Switch" is replaced with "Routes", and with this no need to use "exact", and "components" attribute is replaced with "element", and component is mentioned as "{<Home />}" instead of "{Home}". "Link" is used instead of anchor tags, and "to" attribute is used instead of "href".

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Hello from home page</h1>
+    </>
+  );
+};
+
+export default Home;
