ef4a6619c66357a1dfe6f7dbf850707ed9fb482d
raaghavendrahm <raaghavendrahm@gmail.com>
2021-11-06 12:41:30 +0530

    addToCart function is created for onClick on ADD button. This successfully captures the product which is clicked. Next, logic of how cart should look is defined.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom';
 const Product = ({ product }) => {
+  // Add to cart
+  const addToCart = (e, product) => {
+    e.preventDefault(); // to prevent redirecting to SingleProduct on clicking "ADD"
+    console.log(product);
+  };
+
   return (
     <Link to={`/products/${product._id}`}>
       <div>
@@ -12,7 +18,12 @@ const Product = ({ product }) => {
         </div>
         <div className="flex justify-between items-center mt-4">
           <span>₹ {product.price}</span>
-          <button className="bg-yellow-500 py-1 px-4 rounded-full font-bold">
+          <button
+            className="bg-yellow-500 py-1 px-4 rounded-full font-bold"
+            onClick={(e) => {
+              addToCart(e, product);
+            }}
+          >
             ADD
           </button>
         </div>
