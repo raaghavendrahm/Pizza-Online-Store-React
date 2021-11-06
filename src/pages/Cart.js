3bd066fe585dc7c93c6b83ac4b07c3b2b696537f
raaghavendrahm <raaghavendrahm@gmail.com>
2021-11-06 16:41:27 +0530

    UI for Cart page is done. Values are hardcoded for now. Next, id of products from local storage is used to fetch products from server.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,41 @@
 const Cart = () => {
-  return <div>This is Cart Page</div>;
+  return (
+    <div className="container mx-auto lg:w-1/2 w-full pb-24">
+      <h1 className="my-12 font-bold">Cart Items</h1>
+      <ul>
+        <li className="mb-12">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <img className="h-16" src="/images/peproni.png" alt="" />
+              <span className="font-bold ml-4 w-48">Double Peperoni</span>
+            </div>
+            <div>
+              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+                -
+              </button>
+              <b className="px-4">2</b>
+              <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+                +
+              </button>
+            </div>
+            <span>₹ 500</span>
+            <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">
+              Delete
+            </button>
+          </div>
+        </li>
+      </ul>
+      <hr className="my-6" />
+      <div className="text-right">
+        <b>Grand Total:</b> ₹ 1000
+      </div>
+      <div className="text-right mt-6">
+        <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+          Order Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
