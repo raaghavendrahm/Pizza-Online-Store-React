888c341c72fe64ac8c1c9c4a5e4314472057a214
raaghavendrahm <raaghavendrahm@gmail.com>
2021-11-06 12:00:18 +0530

    CartContext is imported to App and all the components are wrapped inside "CartContext.Provider". "value" parameter is used. Data passed in for "value" will be accessible to all the components wrapped inside "CartContext.Provider". Next, data passed for "value" is accessed in components.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Navbar from './components/Navbar';
 import ProductsPage from './pages/ProductsPage';
 import Cart from './pages/Cart';
 import SingleProduct from './pages/SingleProduct';
+import { CartContext } from './CartContext';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/productspage" element={<ProductsPage />}></Route>
-          <Route path="/products/:_id" element={<SingleProduct />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </div>
+      <CartContext.Provider value={{ name: 'raaghanna' }}>
+        <div className="App">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/productspage" element={<ProductsPage />}></Route>
+            <Route path="/products/:_id" element={<SingleProduct />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </div>
+      </CartContext.Provider>
     </Router>
   );
 }
